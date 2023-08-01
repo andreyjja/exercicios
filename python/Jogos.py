27cf2449511e41387548f20ebdc68c0f89e444b2
andreyjja <48783626+andreyjja@users.noreply.github.com>
2023-07-31 22:28:24 -0300

    Add files via upload

---
+++ python/Jogos.py
@@ -0,0 +1,14 @@
+import Advinhacao
+
+print("###################################")
+print("##########Menu De jogos############")
+print("##########escolha um jogo##########")
+print("[1]Advinhação [2]forca ")
+jogo = int(input("Qual Jogo?  :"))
+print("###################################")
+
+if jogo == 1:
+    print("adivinhação")
+    Advinhacao.Jogar()
+elif jogo == 2:
+    print("forca")
